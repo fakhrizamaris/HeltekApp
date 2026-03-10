a42b951ce2b51743ed4ae7ceac3dd8fe81a9c125
fakhrizamaris <djamarisfakhri@gmail.com>
2026-03-10 21:08:45 +0700

    add daily progress and debug data view

--- HeltekApp/DailyProgress.swift
+++ HeltekApp/DailyProgress.swift
@@ -5,3 +5,20 @@
 //  Created by Fakhri Djamaris on 10/03/26.
 //
 
+import Foundation
+import SwiftData
+
+@Model
+class DailyProgress{
+    var date: Date
+    var completedStretches: Int = 0
+    var isGoalMet: Bool = false
+    
+    var lastUpdated: Date
+    
+    
+    init(date: Date = Date(), lastUpdated: Date = Date()) {
+        self.date = date
+        self.lastUpdated = lastUpdated
+    }
+}
